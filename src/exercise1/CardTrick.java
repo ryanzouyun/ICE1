023db6dafc60758f4cd9557ac8e6fa488b475314
ryanzouyun <123588747+ryanzouyun@users.noreply.github.com>
2023-02-06 16:38:42 -0500

    edited printInfo()

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -45,19 +45,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Yun Zou, but you can call me Ryan");
         System.out.println();
         
-        System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("My 2023 ambitions:");
+        System.out.println("-- Exercise more");
+        System.out.println("-- Meet more friends at school");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Sleeping");
+        System.out.println("-- Eating");
+        System.out.println("-- Hanging out with friends");
+        System.out.println("-- Playing games");
 
         System.out.println();
         
